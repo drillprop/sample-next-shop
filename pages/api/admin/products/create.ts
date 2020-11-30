d2c1704d0614bf78053744bae98c02f2e1c5446e
drillprop <drylbartosz@gmail.com>
2020-11-30 16:31:25 +0100

    add create endpoint

---
+++ pages/api/admin/products/create.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { product } from '../../../../models';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { name, category, priceCents } = req.body;
+  console.log(req.body);
+  const newProduct = await product.create({
+    data: {
+      name,
+      category,
+      priceCents,
+    },
+  });
+  res.json({ product: newProduct });
+};
