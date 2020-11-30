676e6ad325b47ad869450fe66d2013e740d2ca53
drillprop <drylbartosz@gmail.com>
2020-11-30 10:48:47 +0100

    create form in admin product page, add styling

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps } from 'next';
 import { PrismaClient, Product } from '@prisma/client';
+import styles from './Products.module.css';
+import { FormEvent } from 'react';
 const prisma = new PrismaClient();
 
 type Props = {
@@ -7,18 +9,36 @@ type Props = {
 };
 
 const AdminProductsPage = ({ products }: Props) => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
   return (
     <div>
       <h1>Admin products page</h1>
       <ul>
         {products.map((product) => {
           return (
-            <li key={product.id}>
+            <li className={styles.productItem} key={product.id}>
               {product.name} {product.priceCents}
             </li>
           );
         })}
       </ul>
+      <form onSubmit={handleSubmit} className={styles.newProductForm}>
+        <label>
+          Product name
+          <input placeholder='name' type='text' name='name' />
+        </label>
+        <label>
+          Product category
+          <input placeholder='category' type='text' name='category' />
+        </label>
+        <label>
+          Product price
+          <input placeholder='price' type='text' name='price' />
+        </label>
+        <button type='submit'>Add product</button>
+      </form>
     </div>
   );
 };
